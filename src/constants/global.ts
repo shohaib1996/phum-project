072f901192996f6d1bb959723a2186a3f00b7849
shohaib1996 <khanshohaibhossain@gmail.com>
2024-08-20 03:59:54 +0600

    added offercourse

--- src/constants/global.ts
+++ src/constants/global.ts
@@ -22,12 +22,19 @@ export const monthOptions = monthNames.map((item) => ({
   label: item,
 }));
 
+const weekdays = ["Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
+
 export const genderOptions = genders.map((item) => ({
   value: item.toLowerCase(),
   label: item,
 }));
 
 export const bloodGroupOptions = bloodGroups.map((item) => ({
+  value: item,
+  label: item,
+}));
+
+export const weekDaysOptions = weekdays.map((item) => ({
   value: item,
   label: item,
 }));
--- src/redux/features/admin/courseManagement.api.ts
+++ src/redux/features/admin/courseManagement.api.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import {
   TCourse,
   TQueryParam,
@@ -88,6 +89,28 @@ const courseManagementApi = baseApi.injectEndpoints({
       }),
       invalidatesTags: ["courses"],
     }),
+    getCourseFaculties: builder.query({
+      query: (id) => {
+        return {
+          url: `/courses/${id}/get-faculties`,
+          method: "GET",
+        };
+      },
+      transformResponse: (response: TResponseRedux<any>) => {
+        return {
+          data: response.data,
+          meta: response.meta,
+        };
+      },
+    }),
+    createOfferedCourse: builder.mutation({
+      query: (data) => ({
+        url: `offered-courses/create-offered-course`,
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["courses"],
+    }),
   }),
 });
 
@@ -98,4 +121,6 @@ export const {
   useGetAllCoursesQuery,
   useAddCourseMutation,
   useAddFacultiesMutation,
+  useGetCourseFacultiesQuery,
+  useCreateOfferedCourseMutation,
 } = courseManagementApi;
